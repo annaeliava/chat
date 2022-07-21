ea0637ec84ddb658db0b33bf3ddc0271e069bcfb
OlesyaVys <birdsunny@mail.ru>
2022-07-22 00:27:15 +0300

    add folder in project,sep up webpack and ts to js

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const webpack = require('webpack'); 
+
+module.exports = {
+  entry: './client/assets/ts/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
