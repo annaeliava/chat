24ee4fe30e96dd5f4deed89d2e6eb882b495367a
annaeliava <annaeliavad@gmail.com>
2022-07-31 12:11:33 +0200

    added emojiPicker

--- client/assets/ts/emoji.js
+++ client/assets/ts/emoji.js
@@ -3,8 +3,23 @@ import { createPicker } from "picmo";
 console.log('working');
 
 const container = document.querySelector('.pickerContainer');
+const btn = document.getElementById('btn-emoji');
 
 const picker = createPicker({
   rootElement: container
 });
 
+picker.addEventListener('emoji:select', event => {
+  console.log('Emoji selected:', event.emoji);
+  const message = document.getElementById('message-input');
+  message.innerText = event.emoji;
+});
+
+btn.onclick = function(){
+  const p = document.getElementById('emojiPicker');
+  if(p.style.display === "none"){
+    p.style.display = "block";
+  } else {
+    p.style.display = "none";
+  }
+};
